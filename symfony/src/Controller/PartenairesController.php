3cca815cfdfb834a7726891f19ab6c89a58cfc58
yaTTTa <patate_o_gruyere@hotmail.fr>
2022-09-30 21:13:59 +0200

    #3 Intégration dans projet symfony

---
+++ symfony/src/Controller/PartenairesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PartenairesController extends AbstractController
+{
+    /**
+     * @Route("/partenaires")
+     */
+    public function partenaires()
+    {
+        
+    }
+}
