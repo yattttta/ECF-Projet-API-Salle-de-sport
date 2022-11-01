96698353f412c441494cd4ce9cbabea24cccbbf6
yaTTTa <patate_o_gruyere@hotmail.fr>
2022-11-01 02:17:43 +0100

    modif procfile

--- symfony/src/Controller/SallesDesPartenairesController.php
+++ symfony/src/Controller/SallesDesPartenairesController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Login;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
